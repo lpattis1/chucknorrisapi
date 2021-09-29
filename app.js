43b8922e30aa27a92e4d8a998fd5c8d2b8aacb61
lpattis1 <lpattis1@asu.edu>
2021-09-29 12:40:35 -0500

    Finished mini-project.

---
+++ app.js
@@ -0,0 +1,31 @@
+// Variables
+const btn = document.querySelector(".get-jokes");
+const number = document.querySelector("#number");
+const jokeOutput = document.querySelector(".jokes");
+
+// Event
+btn.addEventListener("click", getJokes);
+
+// Get Jokes
+function getJokes(e) {
+  let numSelection = number.value;
+
+  e.preventDefault();
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", `http://api.icndb.com/jokes/random/${numSelection}`, true);
+
+  xhr.onload = function () {
+    let jokeTransfer = JSON.parse(xhr.responseText);
+    let jokeContent = jokeTransfer.value;
+
+    jokeContent.forEach((jokes) => {
+      const newJokeLi = document.createElement("li");
+      newJokeLi.textContent = jokes.joke;
+      jokeOutput.appendChild(newJokeLi);
+    });
+  };
+
+  xhr.send();
+}
